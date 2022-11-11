1fa5c416f7f5f838cd8ccbed5c9383e7e51b28a6
Bhavadharani01 <117000955+Bhavadharani01@users.noreply.github.com>
2022-11-11 22:03:29 +0530

    Week2.day1

---
+++ day1/LearnLaunch.java
@@ -0,0 +1,26 @@
+package week2.day1;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LearnLaunch {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();//setup path
+		ChromeDriver driver = new ChromeDriver();//crt obj fr ChromeDriver
+		driver.manage().window().maximize();//maximize window
+		driver.get("http://leaftaps.com/opentaps/control/login"); //URL to launch
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.findElement(By.id("username")).sendKeys("demosalesmanager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		String title = driver.getTitle();
+		System.out.println(title);
+	}
+   
+   
+}
+
